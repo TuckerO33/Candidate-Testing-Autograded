2b7bfdfa1444de335e70ac0527328877b8ff2974
TuckerO33 <tuckerovermann@gmail.com>
2024-04-03 11:06:03 -0500

    Part 2 done! Tests 1-10 passed

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,35 +6,41 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride"
+let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? " ];
+let correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question("WHAT.. is your name?")
+ candidateName = input.question("WHAT.. is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+  for (let i = 0; i < 5; i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(String(candidateAnswer));
+  }
+  return candidateAnswers
 
 }
 
 function gradeQuiz(candidateAnswers) {
-
+  for (let i = 0; i < 5; i++) {
+console.log(`Question ${i+1} - You answered ${candidateAnswers[i]}. The correct answer is ${correctAnswers[i]}`);
+  }
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("...that's right, fine.")
-} else {
-  console.log("WRONG! *casts you into the Gorge of Eternal Peril*")
-}
+// if (askQuestion() == correctAnswers) {
+//   console.log("...that's right, fine.")
+// } else {
+//   console.log("WRONG! *casts you into the Gorge of Eternal Peril*")
+// }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
